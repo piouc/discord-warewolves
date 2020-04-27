2bb2de4646c8723ce883ce5d681c3fd4eecedff3
piou <piou@hotmail.co.jp>
2020-04-27 19:01:04 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Discord, {Client, Message, Channel, User, TextChannel} from 'discord.js'
 const client = new Client()
 const token = process.env.DISCORD_TOKEN
 
+
 client.on('ready', () => {
   console.log('ready...')
 })
